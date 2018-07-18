3e400fb3f3386714f7e491a55098174c148a2b49
anibalsanchez <anibal.sanchez@extly.com>
2018-07-18 19:13:26 +0200

    DONE

--- src/ios/TwilioVideoPlugin.m
+++ src/ios/TwilioVideoPlugin.m
@@ -12,18 +12,20 @@
 - (void)openRoom:(CDVInvokedUrlCommand*)command {
     NSString* token = command.arguments[0];
     NSString* room = command.arguments[1];
-    
+
     dispatch_async(dispatch_get_main_queue(), ^{
         UIStoryboard *sb = [UIStoryboard storyboardWithName:@"TwilioVideo" bundle:nil];
         TwilioVideoViewController *vc = [sb instantiateViewControllerWithIdentifier:@"TwilioVideoViewController"];
-        
+
         //vc.accessToken = token;
         UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
         [vc.navigationItem setRightBarButtonItem:[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(dismissTwilioVideoController)]];
-         
-        
+
+
         [self.viewController presentViewController:nav animated:YES completion:^{
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"ok"];
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"OK"];
+            [pluginResult setKeepCallbackAsBool:YES];
+
             [vc connectToRoom:room token:token];
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         }];
@@ -33,6 +35,9 @@
 
 - (void) dismissTwilioVideoController {
     [self.viewController dismissViewControllerAnimated:YES completion:nil];
+
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"DONE"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
